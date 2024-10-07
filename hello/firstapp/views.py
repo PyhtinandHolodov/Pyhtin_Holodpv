ce65bc2eaac88349ea7fd9f52c6674be3e788614
Pyhtin_Holodove <rebekafounts@gmail.com>
2024-10-08 00:52:14 +0300

    9ЛЗ

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,5 +1,14 @@
+from django.http import *
 from .forms import UserForm
 from django.shortcuts import render
 def index(request):
- userform = UserForm()
- return render(request, "firstapp/index.html", {"form": userform})
+ if request.method == "POST":
+    userform = UserForm(request.POST)
+    if userform.is_valid():
+        name = userform.cleaned_data["name"]
+        return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
+    else:
+        return HttpResponse("Ошибка ввода данных")
+ else:
+    userform = UserForm()
+    return render(request, "firstapp/index.html", {"form": userform})
